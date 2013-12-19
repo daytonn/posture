b0f1088b8f901bcaf7c3ec91e55fb705a74693bf
Dayton Nolan <daytonn@gmail.com>
2013-12-19 16:17:10 -0600

    Tabs plugin finished

--- spec/views/posture.tabs_spec.js
+++ spec/views/posture.tabs_spec.js
@@ -31,6 +31,23 @@ describe("Posture.Tab", function() {
     it("has a pane element", function() {
       expect(subject.$pane).toBeJqueryWrapped(subject.paneSelector);
     });
+
+    describe("with an existing active tab", function() {
+      beforeEach(function() {
+        loadFixtures("active_tabs.html");
+        var tab = $("#active-tabs .tab.active")[0];
+        subject = new Posture.Tab({
+          el: tab,
+          activeClass: "active",
+          dispatcher: _.clone(Backbone.Events)
+        });
+      });
+
+      it("shows the pane", function() {
+        subject.initialize();
+        expect(subject.$pane).toBeVisible();
+      });
+    });
   });
 
   describe("isActive", function() {
@@ -50,16 +67,24 @@ describe("Posture.Tab", function() {
     it("adds the active class", function() {
       expect(subject.$el).toHaveClass("active");
     });
+
+    it("shows it's pane", function() {
+      expect(subject.$pane).toBeVisible();
+    });
   });
 
   describe("deactivate", function() {
     beforeEach(function() {
-      subject.$el.removeClass(subject.activeClass);
+      subject.activate();
       subject.deactivate();
     });
 
     it("removes the active class", function() {
       expect(subject.$el).not.toHaveClass("active");
+    });
+
+    it("hides it's pane", function() {
+      expect(subject.$pane).not.toBeVisible();
     });
   });
 
@@ -92,7 +117,7 @@ describe("Posture.Tab", function() {
       });
 
       it("triggers an event on the dispatcher", function() {
-        expect(subject.dispatcher.trigger).toHaveBeenCalledWith("posture.tab.activate", subject);
+        expect(subject.dispatcher.trigger).toHaveBeenCalledWith(subject.eventNamespace + ".activate", subject);
       });
     });
   });
@@ -108,10 +133,18 @@ describe("Posture.Tabs", function() {
   });
 
   describe("initialize", function() {
+    beforeEach(function() {
+      spyOn(subject, "activateTab");
+      subject.initialize();
+    });
+
+    it("has an id", function() {
+      expect(subject.id).toBeTypeof(String);
+    });
+
     describe("dispatcher event listeners", function() {
       beforeEach(function() {
-        spyOn(subject, "activateTab");
-        subject.dispatcher.trigger("posture.tab.activate", _(subject.tabViews).first());
+        subject.dispatcher.trigger(subject.eventNamespace("activate"), _(subject.tabViews).first());
       });
 
       it("registers for the tab activate event", function() {
@@ -170,6 +203,61 @@ describe("Posture.Tabs", function() {
   describe("activeTab", function() {
     it("returns the active tab view", function() {
       expect(subject.activeTab()).toEqual(_(subject.tabViews).first());
+    });
+  });
+
+  describe("activeTab", function() {
+    var tab;
+    var currentActiveTab;
+    beforeEach(function() {
+      spyOn(subject.dispatcher, "trigger");
+      spyOn(subject, "trigger");
+      subject.activeTab().deactivate();
+      currentActiveTab = _(subject.tabViews).first();
+      currentActiveTab.activate();
+      tab = _(subject.tabViews).last();
+    });
+
+    it("activates the given tab", function() {
+      expect(tab.isActive()).toBeFalse();
+      subject.activateTab(tab);
+      expect(tab.isActive()).toBeTrue();
+    });
+
+    it("deactivates the current active tab", function() {
+      expect(currentActiveTab.isActive()).toBeTrue();
+      subject.activateTab(tab);
+      expect(currentActiveTab.isActive()).toBeFalse();
+    });
+
+    it("triggers the activated event on the view", function() {
+      subject.activateTab(tab);
+      expect(subject.trigger).toHaveBeenCalledWith("tab.activated", tab);
+    });
+
+    it("triggers the unique posture.tab:(id).activated event", function() {
+      subject.activateTab(tab);
+      expect(subject.dispatcher.trigger).toHaveBeenCalledWith(subject.eventNamespace("activated"), tab);
+    });
+
+    it("triggers the deactivated event on the view", function() {
+      subject.activateTab(tab);
+      expect(subject.trigger).toHaveBeenCalledWith("tab.deactivated", currentActiveTab);
+    });
+
+    it("triggers the unique posture.tab:(id).deactivated event", function() {
+      subject.activateTab(tab);
+      expect(subject.dispatcher.trigger).toHaveBeenCalledWith(subject.eventNamespace("deactivated"), currentActiveTab);
+    });
+  });
+
+  describe("eventNamespace", function() {
+    it("returns the event namespace", function() {
+      expect(subject.eventNamespace()).toEqual("posture.tab:" + subject.id);
+    });
+
+    it("returns the event namespace with a given event", function() {
+      expect(subject.eventNamespace("foo")).toEqual("posture.tab:" + subject.id + ".foo");
     });
   });
 });
--- src/posture.tabs.js
+++ src/posture.tabs.js
@@ -9,53 +9,60 @@
   }
 
   //### Tab
-  // Wrap Tab definition in a closure for private methods and properties
-  (function() {
-    Posture.Tab = Backbone.View.extend({
-      events: {
-        "click": "handleClick"
-      },
+  Posture.Tab = Backbone.View.extend({
+    events: {
+      "click": "handleClick"
+    },
 
-      // Initialize the tab with options from the tabs view
-      initialize: function(options) {
-        // Assign context for private methods
-        setOptions(this, options);
-        this.paneSelector = this.$el.attr("href");
-        this.$pane = $(this.paneSelector);
-      },
+    // Initialize the tab with options from the tabs view
+    initialize: function(options) {
+      // Assign context for private methods
+      setOptions(this, options);
+      this.paneSelector = this.$el.attr("href");
+      this.$pane = $(this.paneSelector);
+      if (this.isActive()) this.$pane.show();
+    },
 
-      // Convenience method to determine if the tab is active
-      isActive: function() {
-        return this.$el.hasClass("active");
-      },
+    // Convenience method to determine if the tab is active
+    isActive: function() {
+      return this.$el.hasClass("active");
+    },
 
-      // Add the active class to the tab
-      activate: function() {
-        this.$el.addClass(this.activeClass);
-      },
+    // Add the active class to the tab
+    activate: function() {
+      this.$el.addClass(this.activeClass);
+      this.$pane.show();
+    },
 
-      // Removes the active class to the tab
-      deactivate: function() {
-        this.$el.removeClass(this.activeClass);
-      },
+    // Removes the active class to the tab
+    deactivate: function() {
+      this.$el.removeClass(this.activeClass);
+      this.$pane.hide();
+    },
 
-      handleClick: function(event) {
-        event.preventDefault();
-        if (!this.isActive()) this.dispatcher.trigger("posture.tab.activate", this);
-      }
-    });
-  })();
+    // Trigger the activate event on click
+    handleClick: function(event) {
+      event.preventDefault();
+      if (!this.isActive()) this.dispatcher.trigger(this.eventNamespace + ".activate", this);
+    }
+  });
 
   //### Tabs
   // Wrap Tabs definition in a closure for private methods and properties
   (function() {
     // Create a tab view for each .tab
+    // this method is private inside the closure
     function createTabViews() {
+      // Keep a local reference to this to use in
+      // the iterator
       var _this = this;
       this.tabViews = [];
+      // Push a new tab view into the tabViews array
+      // for each element matching the tabSelector
       _(this.$el.find(this.tabSelector)).each(function(tab) {
         _this.tabViews.push(new Posture.Tab({
           el: tab,
+          eventNamespace: _this.eventNamespace(),
           activeClass: _this.activeClass,
           dispatcher: _this.dispatcher
         }));
@@ -66,7 +73,11 @@
       // Initialize the view with defaults and
       // cache the elements
       initialize: function(options) {
-        // Assign context for private methods
+        // Create an id for this view
+        this.id = _.uniqueId();
+        // Create empty options object if none passed
+        // then create common defaults for convenience
+        options = options || {};
         this.defaults = {
           tabSelector: ".tab",
           activeClass: "active",
@@ -74,26 +85,41 @@
         };
         setOptions(this, _.defaults(options, this.defaults));
         createTabViews.call(this);
-        this.dispatcher.on("posture.tab.activate", this.activateTab, this);
+        this.dispatcher.on(this.eventNamespace("activate"), this.activateTab, this);
         if (!this.hasActiveTab() && this.tabViews.length) _(this.tabViews).first().activate();
       },
 
+      // Determine if the tabs have an active tab
       hasActiveTab: function() {
         return !!this.activeTab();
       },
 
+      // Find the current active tab
       activeTab: function() {
         return _(this.tabViews).find(function(tabView) {
           return tabView.isActive();
         });
       },
 
+      // Deactivate the currently active tab and trigger
+      // the `posture.tab.deactivated` event, then
+      // activate the given tab, and trigger the
+      // `posture.tab.activated` event
       activateTab: function(activeTab) {
-        console.log("activateTab");
-        // _(this.tabViews).each(function(tabView) {
-        //   tabView.deactivate();
-        // });
-        // activeTab.activate();
+        var currentActiveTab = this.activeTab();
+        currentActiveTab.deactivate();
+        activeTab.activate();
+
+        this.trigger("tab.deactivated", currentActiveTab);
+        this.trigger("tab.activated", activeTab);
+        this.dispatcher.trigger(this.eventNamespace("deactivated"), currentActiveTab);
+        this.dispatcher.trigger(this.eventNamespace("activated"), activeTab);
+      },
+
+      // Convenience method to return the unique event namespace
+      eventNamespace: function(event) {
+        var eventName = event ? "." + event : "";
+        return "posture.tab:" + this.id + eventName;
       }
     });
   })();
